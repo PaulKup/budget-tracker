b0480b0055c65c2481d5a54be4d67100bf313420
ChillyWilly <noreply>
2021-05-24 17:58:58 -0400

    service worker manifest

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -3,9 +3,9 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./public/css/styles.css",
-    "./public/js/index.js"
+    "index.html",
+    "css/styles.css",
+    "js/index.js"
 ];
 
 self.addEventListener('install', function (e) {
